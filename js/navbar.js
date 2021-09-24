c583966f877403fdc8e6f9532617812ae4ad966b
huynhcongphuc <phuchuynh247@gmail.com>
2021-09-24 21:38:28 +0700

    a

---
+++ js/navbar.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $("#footer").load("\\Layout\\layout_footer.html");
+
+    $("#porfolio").click(function () {
+        window.location = "/index.html";
+    })
+    $("#programing").click(function () {
+        window.location = "/page\\programing.html";
+    })
+    $("#automation").click(function () {
+        window.location = "/page\\automation.html";
+    })
+    $("#electrical").click(function () {
+        window.location = "/page\\electrical.html";
+    })
+    $("#network").click(function () {
+        window.location = "/page\\network.html";
+    })
+    $("#protocol").click(function () {
+        window.location = "/page\\protocol.html";
+    })
+});

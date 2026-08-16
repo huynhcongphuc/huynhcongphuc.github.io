42e40556ed019ab71ebe3947433e20dc41c55073
HUỲNH CÔNG PHÚC <91235519+huynhcongphuc@users.noreply.github.com>
2026-08-16 21:02:11 +0700

    Use stable GitHub image sources for product previews

---
+++ js/product-images.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  document.querySelectorAll('.product-preview img').forEach(image=>{
+    const path=image.getAttribute('src');
+    if(path?.startsWith('picture/products/')) image.src=`https://raw.githubusercontent.com/huynhcongphuc/huynhcongphuc.github.io/main/${path}`;
+  });
+});
+
